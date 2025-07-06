fabfcec3d0195e5999a476f7c39d0a70805ab7e2
haquocdat543 <wwwdatha543@gmail.com>
2025-07-06 17:42:10 +0700

    feat(package): add env logger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix::router::router;
+use actix_web::middleware::Logger;
 use actix_web::web::Data;
 use actix_web::{App, HttpServer};
+use env_logger::Env;
 use sea_orm::Database;
 use std::env;
 
@@ -15,8 +17,11 @@ async fn main() -> std::io::Result<()> {
 
     let db_data = Data::new(db); // ✅ Wrap it here
 
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default()) // ✅ this is from actix_web::middleware
             .app_data(db_data.clone()) // ✅ Share it with app
             .service(router::api_scope(db_data.clone())) // Mount /api
     })
