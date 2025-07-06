5bcf30f96fc470484e9851c4230e53c16bebbfc4
haquocdat543 <wwwdatha543@gmail.com>
2025-07-06 11:56:44 +0700

    refactor(mod/user): move (get users) to {controller}

--- src/module/user/handler.rs
+++ src/module/user/handler.rs
@@ -1,6 +1,6 @@
 use actix_web::web;
 
-use super::controller::get_users;
+use super::controller::get_users::get_users;
 
 pub fn user_scope() -> actix_web::Scope {
     web::scope("/user").service(get_users)
