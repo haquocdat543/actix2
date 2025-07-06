4c58fa50174b915390de90fcd4f1ac72a1afb9ea
haquocdat543 <wwwdatha543@gmail.com>
2025-07-06 12:01:47 +0700

    feat(package): add seaorm

---
+++ src/module/user/entity.rs
@@ -0,0 +1,16 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, DeriveEntityModel)]
+#[sea_orm(table_name = "user")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub username: String,
+    pub email: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
+
--- src/module/user/mod.rs
+++ src/module/user/mod.rs
@@ -1,2 +1,4 @@
 pub mod controller;
 pub mod handler;
+
+pub mod entity;
