9c85c9c5fc4847d8649dfd21b4421a4bac7fc2c1
haquocdat543 <wwwdatha543@gmail.com>
2025-07-06 14:45:48 +0700

    feat(mod/user): dto - add validation

--- src/module/user/controller/create.rs
+++ src/module/user/controller/create.rs
@@ -1,12 +1,18 @@
 use crate::module::user::{dto::CreateUserDTO, service::UserService};
 use actix_web::{HttpResponse, Responder, post, web};
 use sea_orm::DatabaseConnection;
+use validator::Validate;
 
 #[post("")]
 pub async fn create(
     db: web::Data<DatabaseConnection>,
     req: web::Json<CreateUserDTO>,
 ) -> impl Responder {
+
+    if let Err(errors) = req.validate() {
+        return HttpResponse::BadRequest().json(errors);
+    }
+
     let req = req.into_inner();
     match UserService::register_user(db.get_ref(), req.name, req.email, req.password).await {
         Ok(user) => HttpResponse::Created().json(user),
--- src/module/user/dto.rs
+++ src/module/user/dto.rs
@@ -1,8 +1,22 @@
 use serde::Deserialize;
+use validator::Validate;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, Validate)]
 pub struct CreateUserDTO {
+    #[validate(length(
+        min = 3,
+        max = 20,
+        message = "Name must be between 3 and 20 characters"
+    ))]
     pub name: String,
+
+    #[validate(email(message = "Email must be a valid email address"))]
     pub email: String,
+
+    #[validate(length(
+        min = 8,
+        max = 20,
+        message = "Name must be between 8 and 20 characters"
+    ))]
     pub password: String,
 }
